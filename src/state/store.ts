ebd669040649fdd114e624b3dcc5e741c7595c6a
KAMADO <abdonsila2004@gmail.com>
2024-05-16 14:46:19 +0100

    chore: Add Consultation slices for CRUD operations

--- src/state/store.ts
+++ src/state/store.ts
@@ -9,16 +9,28 @@ import UpdateSlice from "./Users/UpdateSlice";
 import PatientAuthSlice from "./auth/PatientAuthSlice";
 import DoctorAuthSlice from "./auth/DoctorAuthSlice";
 import AdminAuthSlice from "./auth/AdminAuthSlice";
+// --------------- Consultations Slices ----------------
+import getConsultationSlice from "./Consultation/GetSlice";
+import CreateConsultationSlice from "./Consultation/CreateSlice";
+import UpdateConsultationSlice from "./Consultation/UpdateSlice";
+import DeleteConsultationSlice from "./Consultation/DeleteSlice";
+
 
 export const store = configureStore({
   reducer: {
     patientAuth: PatientAuthSlice,
     doctorAuth: DoctorAuthSlice,
     adminAuth: AdminAuthSlice,
+    // Employees
     getUsers: getSlice,
     createUser: CreateSlice,
     updateUser: UpdateSlice,
     deleteUser: DeleteSlice,
+    // Consultations
+    getConsultations: getConsultationSlice,
+    createConsultation: CreateConsultationSlice,
+    updateConsultation: UpdateConsultationSlice,
+    deleteConsultation: DeleteConsultationSlice,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
